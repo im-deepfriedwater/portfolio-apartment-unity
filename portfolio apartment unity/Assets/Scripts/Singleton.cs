1f951401ce1e86eb8ce7ec2bf59fa543370ad1ff
im-deepfriedwater <justintorres56@gmail.com>
2024-01-06 01:19:05 -0800

    web setup & basic movement tutorial

---
+++ portfolio apartment unity/Assets/Scripts/Singleton.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Singleton : MonoBehaviour
+{
+    public static Singleton Instance { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
